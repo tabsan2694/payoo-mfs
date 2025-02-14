d7a097e973ab87354f330cc09971a863e4a6ad66
tabsan2694 <tabsan2694@gmail.com>
2025-02-15 04:59:21 +0600

    added 4th commit

--- login.js
+++ login.js
@@ -10,4 +10,18 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     // step:03 Get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
     console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+    // step-04: validate phone and pin
+
+    if(phoneNumber === '5' && pinNumber === '1234')
+    {
+        console.log('You are logged in');
+        window.location.href = '/home.html'
+        // step-05: allow user to use the website
+    }
+    else{
+        alert('Wrong Phone Number or Pin');
+    }
 })
