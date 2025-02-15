725cdf6c7b9962846a0c50e4caeb60a724f1cf51
tabsan2694 <tabsan2694@gmail.com>
2025-02-15 06:48:07 +0600

    added 7th commit

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     if(phoneNumber === '5' && pinNumber === '1234')
     {
         console.log('You are logged in');
-        window.location.href = 'home.html'
+        window.location.assign = 'home.html'
         // step-05: allow user to use the website
     }
     else{
