95b4f14d18c4f34082321ce9be25b80eee0993e6
tabsan2694 <tabsan2694@gmail.com>
2025-02-15 09:13:32 +0600

    added 9th commit

--- js/home.js
+++ js/home.js
@@ -11,4 +11,29 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput)
+
+    // step-03: verify the pin number
+
+    if(pinNumberInput === '1234'){
+        console.log('Adding money to your account')
+        // step-04: get the current balance
+        const balance = document.getElementById('account-balance').innerText
+        console.log(balance)
+
+        // step-05: add addMoneyInput with balance
+       const addMoneyNumber = parseFloat(addMoneyInput)
+       const balanceNumber = parseFloat(balance)
+       const newBalance = addMoneyNumber + balanceNumber
+       console.log(newBalance)
+
+        // step-05: update the balance in the UI/Dom
+        document.getElementById('account-balance').innerText = newBalance
+    }
+    else{
+        console.log('Failed to add money. Please try again')
+    }
+
+   
+
+
 })
